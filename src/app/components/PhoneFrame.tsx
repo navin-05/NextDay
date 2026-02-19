e3c8e1e48465aae1c41e0819cd5e7f52feb71720
navin-05 <navin.j2022cse@sece.ac.in>
2026-02-19 15:56:39 +0530

    new 2

--- src/app/components/PhoneFrame.tsx
+++ src/app/components/PhoneFrame.tsx
@@ -5,15 +5,30 @@ export function PhoneFrame() {
     <div
       style={{
         width: "100%",
-        minHeight: "100vh",
+        height: "100vh",
         background: "#0D0D0D",
         fontFamily: "'Inter', -apple-system, BlinkMacSystemFont, 'SF Pro Display', sans-serif",
         display: "flex",
         flexDirection: "column",
+        position: "relative",
+        overflow: "hidden",
       }}
     >
-      {/* Screen Content - Full Screen */}
-      <div style={{ flex: 1, width: "100%", background: "#0D0D0D" }}>
+      {/* Screen Content - Full Screen, Responsive */}
+      <div
+        style={{
+          flex: 1,
+          width: "100%",
+          height: "100%",
+          background: "#0D0D0D",
+          display: "flex",
+          flexDirection: "column",
+          overflowY: "auto",
+          overflowX: "hidden",
+          WebkitOverflowScrolling: "touch",
+          scrollBehavior: "smooth",
+        }}
+      >
         <Outlet />
       </div>
     </div>
